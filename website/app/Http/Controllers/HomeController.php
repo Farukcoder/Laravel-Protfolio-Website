ed63af9504dd931634591e4fc190594ff92e45ba
Farukcoder <67540985+Farukcoder@users.noreply.github.com>
2021-06-26 15:51:58 +0600

    web visitor db and added done
    web visitor db and added done

---
+++ website/app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\VisitorModel;
+
+class HomeController extends Controller
+{
+    public function HomeIndex(){
+
+        $UserIp = $_SERVER['REMOTE_ADDR'];
+
+        date_default_timezone_set("Asia/Dhaka");
+
+        $timeDate = date("Y-m-d h:i:sa");
+
+        VisitorModel::insert(['ip_address'=>$UserIp,'visit_time'=>$timeDate]);
+
+        return view('home');
+    }
+}
